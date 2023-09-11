f22aa9d89685e541e91a4512ecaa0498bbbd7ab2
KarenGilces <you@example.com>
2023-09-11 13:49:57 -0500

    ssssssssssssssssssssssss

--- models/MarcaModel.js
+++ models/MarcaModel.js
@@ -22,4 +22,43 @@ export const MarcaModel = sequelize.define(
   {
     timestamps: false,
   }  
-  );
+  );const marcasData = [
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    { descripcion: "VOLKSWAGEN" },
+    { descripcion: "RENAULT" },
+    // Agrega más objetos para insertar más datos
+  ];
+  async function seed() {
+    try {
+      await sequelize.sync({ force: false }); // Esto eliminará y recreará la tabla (ten cuidado en producción)
+  
+      for (const marcaData of marcasData) {
+        await MarcaModel.create(marcaData);
+      }
+  
+      console.log("Datos de marcas insertados con éxito.");
+    } catch (error) {
+      console.error("Error al insertar datos de marcas:", error);
+    } finally {
+      //sequelize.close(); // Cierra la conexión a la base de datos al finalizar
+    }
+  }
+  
+  seed();
